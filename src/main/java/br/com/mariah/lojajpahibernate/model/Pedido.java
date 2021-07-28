46df00db9b244c8eac47762216466c7e7155fdb1
matheus <matheusmartinsdacosta21@gmail.com>
2021-07-28 00:00:53 -0300

    Mapping entities Cliente and Produto

---
+++ src/main/java/br/com/mariah/lojajpahibernate/model/Pedido.java
@@ -0,0 +1,72 @@
+package br.com.mariah.lojajpahibernate.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "PEDIDOS")
+public class Pedido {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private BigDecimal valorTotal;
+	private LocalDate data = LocalDate.now();
+
+	@ManyToOne
+	private Cliente cliente;
+
+	public Pedido() {
+	}
+
+	public Pedido(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public BigDecimal getValorTotal() {
+		return valorTotal;
+	}
+
+	public void setValorTotal(BigDecimal valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	@Override
+	public String toString() {
+		return "Pedido [id=" + id + ", valorTotal=" + valorTotal + ", data=" + data + ", cliente=" + cliente + "]";
+	}
+
+	
+	
+}
