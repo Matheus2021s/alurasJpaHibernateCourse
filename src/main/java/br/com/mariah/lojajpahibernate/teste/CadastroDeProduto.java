443f08d4d1445d16b93872c6168bc73fea8b7b42
matheus <matheusmartinsdacosta21@gmail.com>
2021-07-27 22:13:47 -0300

    Create Categoria model and categoriaDAO to persist and try first example of relationship between models

--- src/main/java/br/com/mariah/lojajpahibernate/teste/CadastroDeProduto.java
+++ src/main/java/br/com/mariah/lojajpahibernate/teste/CadastroDeProduto.java
@@ -3,29 +3,28 @@ package br.com.mariah.lojajpahibernate.teste;
 import java.math.BigDecimal;
 
 import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.EntityTransaction;
-import javax.persistence.Persistence;
 
+import br.com.mariah.lojajpahibernate.dao.CategoriaDAO;
 import br.com.mariah.lojajpahibernate.dao.ProdutoDAO;
+import br.com.mariah.lojajpahibernate.model.Categoria;
 import br.com.mariah.lojajpahibernate.model.Produto;
 import br.com.mariah.lojajpahibernate.util.JPAUtil;
 
 public class CadastroDeProduto {
 
 	public static void main(String[] args) {
-		Produto celular = new Produto();
-		celular.setNome("xiaomi");
-		celular.setDescricao("mobile");
-		celular.setPreco(new BigDecimal(1000));
-		
-		
 		EntityManager entityManager = JPAUtil.getEntityManager();
 		
+		Categoria celurares = new Categoria("CELULARES");		
+		Produto celular = new Produto("XIAOMI","MOBILE",new BigDecimal(1000), celurares);
+		
+		
+		CategoriaDAO categoriaDAO = new CategoriaDAO(entityManager);
 		ProdutoDAO produtoDAO = new ProdutoDAO(entityManager);
 		
 		entityManager.getTransaction().begin();
 		
+		categoriaDAO.cadastrar(celurares);
 		produtoDAO.cadastrar(celular);
 		
 		entityManager.getTransaction().commit();
