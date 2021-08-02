e7b5e79de4f94486e69216e1764344462240913a
matheus <matheusmartinsdacosta21@gmail.com>
2021-08-02 00:50:54 -0300

    Trying criteria API to generate query without jpql. Common use to optional paramertes in query filters

--- src/main/java/br/com/mariah/lojajpahibernate/dao/ProdutoDAO.java
+++ src/main/java/br/com/mariah/lojajpahibernate/dao/ProdutoDAO.java
@@ -1,9 +1,14 @@
 package br.com.mariah.lojajpahibernate.dao;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 import br.com.mariah.lojajpahibernate.model.Produto;
 
@@ -56,5 +61,29 @@ public class ProdutoDAO {
 		return this.entityManager.createQuery(jpql,BigDecimal.class)
 				.setParameter("nome", nome)
 				.getSingleResult();
+	}
+	
+	
+	public List<Produto> buscarPorParametrosComCriteria(String nome, BigDecimal preco, LocalDate dataCadastro) {
+		CriteriaBuilder criteriaBuilder = this.entityManager.getCriteriaBuilder();
+		CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+		Root<Produto> from = criteriaQuery.from(Produto.class);
+		Predicate filtros = criteriaBuilder.and();
+
+		
+		if (nome != null && !nome.trim().isEmpty()) {
+			filtros = criteriaBuilder.and(filtros, criteriaBuilder.equal(from.get("nome"), nome));
+		}
+		if (preco != null) {
+			filtros = criteriaBuilder.and(filtros, criteriaBuilder.equal(from.get("preco"), preco));
+		}
+		if (dataCadastro != null) {
+			filtros = criteriaBuilder.and(filtros, criteriaBuilder.equal(from.get("dataCadastro"), dataCadastro));
+		}
+		
+		criteriaQuery.where(filtros);
+		
+		return entityManager.createQuery(criteriaQuery).getResultList();
+
 	}
 }
